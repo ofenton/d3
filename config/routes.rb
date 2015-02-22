9be255f23eb2a2bc7041113b3524b0f630fb6410
ofenton <ofenton@yahoo-inc.com>
2015-02-22 13:35:23 +0000

    add files

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
-  get 'graph_controller/index'
-
-  Rails.application.routes.draw do
-    get 'graph/index'
-    get 'graph/data', :defaults => { :format => 'json' }
-  end
+  get 'graph/index'
+  get 'graph/data', :defaults => { :format => 'json' }
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
